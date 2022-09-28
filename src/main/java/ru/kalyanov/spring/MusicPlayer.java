ab09ed3e21eac13ddacbacc41fe9a96678280d79
Tikkimik <googlbubu@gmail.com>
2022-09-28 11:55:15 +0300

    Dependency Injection (DI) 2st

--- src/main/java/ru/kalyanov/spring/MusicPlayer.java
+++ src/main/java/ru/kalyanov/spring/MusicPlayer.java
@@ -1,14 +1,47 @@
 package ru.kalyanov.spring;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    //инверсия управления
-    public MusicPlayer(Music music) {
-        this.music = music;
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    //инверсия управления
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+
+    public MusicPlayer() {}
+
     public void  playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for(Music m : musicList){
+            System.out.println("Playing: " + m.getSong());
+        }
     }
 }
--- src/main/java/ru/kalyanov/spring/testSpring.java
+++ src/main/java/ru/kalyanov/spring/testSpring.java
@@ -9,12 +9,12 @@ public class testSpring {
         );
 
        // Music music = context.getBean("musicBean", Music.class);
-
        // MusicPlayer musicPlayer = new MusicPlayer(music);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
